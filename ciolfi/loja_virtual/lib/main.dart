e3a71a4bca267af39f86bfa316d9e13e23376be3
Ivonei Balena <iibalena@gmail.com>
2020-01-08 10:48:06 -0300

    update

--- ciolfi/loja_virtual/lib/main.dart
+++ ciolfi/loja_virtual/lib/main.dart
@@ -7,31 +7,30 @@ import 'package:scoped_model/scoped_model.dart';
 import 'models/cart_model.dart';
 import 'models/user_model.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
       model: UserModel(),
-      child: ScopedModelDescendant<UserModel>( //esse é o descendente do ScopedModel do usuário, ao altear o model do usuário refaz todo o descendant
+      child: ScopedModelDescendant<UserModel>(
+        //esse é o descendente do ScopedModel do usuário, ao altear o model do usuário refaz todo o descendant
         builder: (context, child, model) {
-          return ScopedModel<CartModel> (
-              model: CartModel(model),
-              child: MaterialApp(
-                title: 'Flutter Clothing',
-                theme: ThemeData(
-                  primarySwatch: Colors.blue,
-                  primaryColor: Color.fromARGB(255, 4, 125, 141),
-                ),
-                debugShowCheckedModeBanner: false,
-                home: HomeScreen(),
-              )
+          return ScopedModel<CartModel>(
+            model: CartModel(model),
+            child: MaterialApp(
+              title: 'Flutter Clothing',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+                primaryColor: Color.fromARGB(255, 4, 125, 141),
+              ),
+              debugShowCheckedModeBanner: false,
+              home: HomeScreen(),
+            ),
           );
         },
-      )
+      ),
     );
   }
 }
