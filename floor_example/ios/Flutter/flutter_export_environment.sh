a2ec6471b95bb3cd353505a59c3b37db6c213fee
Ivonei Balena <iibalena@gmail.com>
2020-02-03 22:33:53 -0300

    flutter example clear

--- floor_example/ios/Flutter/flutter_export_environment.sh
+++ floor_example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ivoneibalena/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ivoneibalena/flutterstudy/floor/example"
-export "FLUTTER_TARGET=/Users/ivoneibalena/flutterstudy/floor/example/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/ivoneibalena/flutterstudy/floor_example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/ivoneibalena/Development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.1.0"
 export "FLUTTER_BUILD_NUMBER=0.1.0"
-export "TRACK_WIDGET_CREATION=true"
