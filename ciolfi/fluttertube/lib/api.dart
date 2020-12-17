259bd5e5d61d055459488a8152c899f7ac99e5ba
Ivonei Balena <iibalena@gmail.com>
2020-12-16 22:07:58 -0300

    commit inicial

---
+++ ciolfi/fluttertube/lib/api.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'models/video.dart';
+
+const API_KEY = "AIzaSyCJ6RMHKc0DrkfiUGvMpbpGeZu3M0fdoYA";
+
+class Api {
+  search(String search) async {
+    http.Response response = await http.get(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10");
+
+    decode(response);
+  }
+
+  List<Video> decode(http.Response response) {
+    if (response.statusCode == 200) {
+      var decoded = json.decode(response.body);
+
+      List<Video> videos = decoded["items"].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+
+      return videos;
+    } else {
+      throw Exception("Faild to load videos");
+    }
+  }
+}
+
+//"https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken"
---
+++ ciolfi/fluttertube/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertube/api.dart';
+import 'package:fluttertube/screens/home.dart';
+
+void main() {
+  Api api = Api();
+  api.search("eletro");
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fluttertube',
+      home: Home(),
+    );
+  }
+}
