696828f9047f6230ab7d9e3c2e049a89e8eee62c
Ivonei Balena <iibalena@gmail.com>
2021-01-26 11:25:51 -0300

    Até 116

--- lechetta/carros/lib/pages/carro/home_page.dart
+++ lechetta/carros/lib/pages/carro/home_page.dart
@@ -2,6 +2,7 @@ import 'package:carros/drawer_list.dart';
 import 'package:carros/pages/carro/carro.dart';
 import 'package:carros/pages/carro/carros_api.dart';
 import 'package:carros/pages/carro/carros_listview.dart';
+import 'package:carros/utils/prefs.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,29 +10,51 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage>{
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initTabs();
+  }
+
+  _initTabs() async {
+    _tabController = TabController(length: 3, vsync: this);
+
+    int tabIndex = await Prefs.getInt("tabIndex");
+    _tabController.index = tabIndex;
+
+    _tabController.addListener(() {
+      Prefs.setInt("tabIndex", _tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Carros"),
-          bottom: TabBar(
-            tabs: [
-              Tab(text: "Clássicos"),
-              Tab(text: "Esportivos"),
-              Tab(text: "Luxo"),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: "Clássicos"),
+            Tab(text: "Esportivos"),
+            Tab(text: "Luxo"),
+          ],
         ),
-        body: TabBarView(children: [
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
           CarrosListView(TipoCarro.classicos),
           CarrosListView(TipoCarro.esportivos),
           CarrosListView(TipoCarro.luxo),
-        ],),
-        drawer: DrawerList(),
+        ],
       ),
+      drawer: DrawerList(),
     );
   }
 }
