f11aaa35e4614afdc0e4784937f7620893b04e93
Ivonei Balena <iibalena@gmail.com>
2021-03-17 21:05:28 -0300

    update card

--- lechetta/carros/lib/pages/carros/carros_listview.dart
+++ lechetta/carros/lib/pages/carros/carros_listview.dart
@@ -19,49 +19,58 @@ class CarrosListView extends StatelessWidget {
         itemBuilder: (context, index) {
           Carro c = carros[index];
 
-          return Card(
-            color: Colors.grey[100],
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: CachedNetworkImage(
-                      imageUrl: c.urlFoto ??
-                          "https://cdn5.vectorstock.com/i/1000x1000/64/29/generic-brown-sedan-car-isolated-on-white-vector-13216429.jpg",
-                    ),
-                  ),
-                  Text(
-                    c.nome ?? "Carro sem nome",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  Text(
-                    "descrição.. ",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  ButtonBarTheme(
-                    data: ButtonBarTheme.of(context),
-                    child: ButtonBar(
-                      children: <Widget>[
-                        FlatButton(
-                          child: const Text('DETALHES'),
-                          onPressed: () => _onClickCarro(context, c),
+          return Container(
+            height: 380,
+            child: InkWell(
+              onTap: () {
+                _onClickCarro(context, c);
+              },
+              child: Card(
+                color: Colors.grey[100],
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: CachedNetworkImage(
+                          height: 280,
+                          imageUrl: c.urlFoto ??
+                              "https://cdn5.vectorstock.com/i/1000x1000/64/29/generic-brown-sedan-car-isolated-on-white-vector-13216429.jpg",
                         ),
-                        FlatButton(
-                          child: const Text('SHARE'),
-                          onPressed: () {
-                            /* ... */
-                          },
+                      ),
+                      Text(
+                        c.nome ?? "Carro sem nome",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Text(
+                        "descrição.. ",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      ButtonBarTheme(
+                        data: ButtonBarTheme.of(context),
+                        child: ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                              child: const Text('DETALHES'),
+                              onPressed: () => _onClickCarro(context, c),
+                            ),
+                            FlatButton(
+                              child: const Text('SHARE'),
+                              onPressed: () {
+                                /* ... */
+                              },
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           );
